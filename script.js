1fffda4d63f3f2f38752e3eacc7ca472c3e99d9b
estilara <80065122+estilara@users.noreply.github.com>
2021-03-23 15:00:34 -0300

    Links, Responsive Settings, Video Test
    new javascript, updated the menu links. testing embeded video vs root video

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 // Video Gallery
 var video = document.querySelectorAll('video')
-    video.forEach(play => play.addEventListener('click', () =>{
-        play.classList.toggle('active');
+video.forEach(play => play.addEventListener('click', () =>{
         if (play.paused){
+            play.setAttribute('controls', true);
             play.play();
-        }else{
+        } else {
             play.pause();
-            play.currentTime = 0;
         }
     }));
+
+  
 
 // Search Bar
 function searchFunction() {
@@ -16,16 +17,40 @@ function searchFunction() {
     input = document.getElementById('myinput');
     filter = input.value.toUpperCase();
     container = document.getElementsByClassName('video-container');
-    video = document.getElementsByClassName('video');
+    video = document.getElementsByClassName('video-wrapper');
 
     for(i=0 ; i< video.length; i++){
         a = video[i].getElementsByClassName('wrapper-credit')[0];
         if(a.innerHTML.toUpperCase().indexOf(filter) > -1){
             video[i].style.display = "";
-        }
-
-        else{
+        } else {
             video[i].style.display = 'none';
         }
     }
+}
+
+// Copy button email
+let copyBtn = document.getElementById('buttonEmail');
+copyBtn.addEventListener('click', copyText);
+
+function copyText(){
+  let div = document.getElementById('email');
+  let text = div.innerText;
+  let textArea  = document.createElement('textarea');
+  textArea.width  = "1px"; 
+  textArea.height = "1px";
+  textArea.background =  "transparents" ;
+  textArea.value = text;
+  document.body.append(textArea);
+  textArea.select();
+  document.execCommand('copy');   //No i18n
+  document.body.removeChild(textArea);
+
+  var tooltip = document.getElementById("myTooltip");
+  tooltip.innerHTML = "Copied";
+}
+
+// Instagram link
+function Open() {
+    window.open("https://www.instagram.com/mlkbrutal", "_blank");
 }
